bee32d016830d47be54102d0bfb04da5dd57a1ac
luis-mendes018 <lms.kracker08@gmail.com>
2025-03-24 17:41:05 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null) 
+                    {
+                        string dados_previsao = "";
+                        dados_previsao += $"Latitude: {t.lat}m/s\n";
+                        dados_previsao += $"Longitude: {t.lon}m/s\n";
+                        dados_previsao += $"Cidade: {txt_cidade.Text}\n";
+                        dados_previsao += $"Temperatura Mínima: {t.temp_min}°C\n";
+                        dados_previsao += $"Temperatura Máxima: {t.temp_max}°C\n";
+                        dados_previsao += $"Velocidade do Vento: {t.speed}m/s\n";
+                        dados_previsao += $"Visibilidade: {t.visibility}m\n";
+                        dados_previsao += $"Nascer do Sol: {t.sunrise}\n";
+                        dados_previsao += $"Pôr do Sol: {t.sunset}\n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Cidade não encontrada";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+
+}
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,58 @@
+﻿using MauiAppTempoAgora.Models;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services;
+
+public class DataService
+{
+   public static async Task<Tempo?> GetPrevisao(string cidade)
+   {
+        Tempo? t = null;
+
+        string chave = "ea15fe6f65a68b3d7a2223282180a6bd";
+
+        string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave}&units=metric";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lon = (double)rascunho["coord"]["lon"],
+                        lat = (double)rascunho["coord"]["lat"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    };
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+            return t;
+   }
+}
